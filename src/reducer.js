296a5f8feea9320fdfa4a2cc00e1e6b4a1c44628
Emmanuel Egomson <fegomson@gmail.com>
2021-05-25 23:02:38 -0500

    Decrease item in cart

--- src/reducer.js
+++ src/reducer.js
@@ -11,6 +11,14 @@ const reducer = (state = initialState, action) => {
       })
       return {...state, cart: tempCart}
 
+      case "DECREASE":
+        let tempCartReduced = state.cart.map(item => {
+          if (item.id === action.payload) {
+            return { ...item, amount: item.amount - 1 }
+          }
+          return item
+        }).filter(item => item.amount !== 0)
+        return {...state, cart: tempCartReduced}
     
 
     case "REMOVE_ITEM":
